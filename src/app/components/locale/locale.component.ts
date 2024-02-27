e38d564b18dfa06b59ba1befcd55348ceeaa7d0b
Gustavo Pasqua <dev.gustavo@outlook.com>
2024-02-26 21:21:51 -0300

    feat: Criado componente locale

---
+++ src/app/components/locale/locale.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-locale',
+  standalone: true,
+  imports: [],
+  templateUrl: './locale.component.html',
+  styleUrl: './locale.component.scss'
+})
+export class LocaleComponent {
+
+}
