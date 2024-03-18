ec6cb3f41dc302f0be55b563fbcc19c32ebb7477
fercarneir0 <dev.luisfernando12@gmail.com>
2024-03-18 14:26:36 -0300

    feat: Adicionando a troca de tela

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
+  providers: [NgModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  container: boolean = true;
 
+  trocaCard(){
+    this.container = !this.container;
+  }
 }
+
+
