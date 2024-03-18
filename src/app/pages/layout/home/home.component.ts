7c4fe4c4de0b7ef3e59a5b5ac1900313b111d0ff
Gustavo Pasqua <dev.gustavo@outlook.com>
2024-03-18 03:49:25 -0300

    feat: Início do desenvolvimento do componente Home

--- src/app/pages/layout/home/home.component.ts
+++ src/app/pages/layout/home/home.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, CommonModule],
+  providers: [NgModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent{
+
+  classCardContainer :boolean = true
+
+  funcao(){
+    this.classCardContainer = !this.classCardContainer    
+  }
 
 }
