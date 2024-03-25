701ebfcfe3477519fc7a7ad437d25a478ab6f2c4
Gustavo Pasqua <dev.gustavo@outlook.com>
2024-03-25 13:29:26 -0300

    fix: Ajustados alguns botões de rotas

--- src/app/pages/layout/navroutes.ts
+++ src/app/pages/layout/navroutes.ts
@@ -1,5 +1,6 @@
 export const dataSource = [
     {icone: 'home', texto: 'Início', rota: 'home'},
-    {icone: 'description', texto: 'Relatórios', rota: 'relatorios'},
+    {icone: 'description', texto: 'Relatórios de Gastos', rota: 'relatorios'},
+    {icone: 'description', texto: 'Relatórios de Doações', rota: 'relatorios'},
     {icone: 'group_add', texto: 'Cadastro admin', rota: 'cadastro-admin'}
 ]
