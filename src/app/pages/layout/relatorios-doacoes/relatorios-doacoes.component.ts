6f84a7452d0939e65c5f019603418824ad845860
Cesarhsl <cesar.henrique1620@gmail.com>
2024-03-25 23:45:58 -0300

    feat/Criação do componente relatório de doações

---
+++ src/app/pages/layout/relatorios-doacoes/relatorios-doacoes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-relatorios-doacoes',
+  standalone: true,
+  imports: [],
+  templateUrl: './relatorios-doacoes.component.html',
+  styleUrl: './relatorios-doacoes.component.scss'
+})
+export class RelatoriosDoacoesComponent {
+
+}
