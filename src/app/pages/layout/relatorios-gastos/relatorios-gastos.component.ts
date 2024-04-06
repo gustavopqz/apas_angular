6906bbcd3be3cc942690b6d32e470f896910319e
César Henrique Sousa Lima <cesar.henrique1620@gmail.com>
2024-04-06 18:57:09 -0300

    feat: Desenvolvimento do componente finalizado (Front)

--- src/app/pages/layout/relatorios-gastos/relatorios-gastos.component.ts
+++ src/app/pages/layout/relatorios-gastos/relatorios-gastos.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { Gastos } from '../../../modules/gastos.module';
 
 @Component({
   selector: 'app-relatorios-gastos',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule, MatTableModule],
   templateUrl: './relatorios-gastos.component.html',
   styleUrl: './relatorios-gastos.component.scss'
 })
-export class RelatoriosGastosComponent {
 
-}
+export class RelatoriosGastosComponent {
+  displayedColumns: string[] = ['data', 'descricao', 'valor'];
+  dataSource: Gastos[] = [
+    { data: '2024-04-01', descricao: 'Ração', valor: 'R$ 680,00' },
+    { data: '2024-04-02', descricao: 'Medicamentos', valor: 'R$ 450,00' },
+    { data: '2024-04-02', descricao: 'Aluguel', valor: 'R$ 700,00' },
+    { data: '2024-04-02', descricao: 'Embasa', valor: 'R$ 425,00' },
+    { data: '2024-04-02', descricao: 'Coelba', valor: 'R$ 500,00' }
+  ]
+ }
+
