34ff91b8c76cca236d486b8576d9de44d729a2d0
Gustavoo-S <Gullsanttos7890@gmail.com>
2024-04-09 14:55:05 -0300

    feito o componente de home, discutido com o CEO

--- src/app/pages/layout/home/home.component.ts
+++ src/app/pages/layout/home/home.component.ts
@@ -1,21 +1,17 @@
-import { Component, OnInit, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatCardModule } from '@angular/material/card'
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule, CommonModule],
-  providers: [NgModule],
+  imports: [MatIconModule, MatButtonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent{
-
-  classCardContainer :boolean = true
-
-  funcao(){
-    this.classCardContainer = !this.classCardContainer    
-  }
+export class HomeComponent {
 
 }
