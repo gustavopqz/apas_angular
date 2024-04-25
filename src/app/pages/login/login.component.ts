cc10493a0271e5ab9f9ee9b82b5174bd20a93bf0
Gustavo Pasqua <dev.gustavo@outlook.com>
2024-04-25 19:00:13 -0300

    fix/feat: Adicionado botão de voltar ao site na tela de login / Adicionado redirecionamento para painel administrativo após o login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,12 @@
 import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, RouterModule],
   providers: [NgModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
