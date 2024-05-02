e122a158c76d6520dc5e361ead1701f969554e6b
fercarneir0 <dev.luisfernando12@gmail.com>
2024-05-01 23:29:01 -0300

    feat: Criando component de esqueci minha senha

---
+++ src/app/pages/esqueci-senha/esqueci-senha.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-esqueci-senha',
+  standalone: true,
+  imports: [],
+  templateUrl: './esqueci-senha.component.html',
+  styleUrl: './esqueci-senha.component.scss'
+})
+export class EsqueciSenhaComponent {
+
+}
