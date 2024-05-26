bfd0a419209dcc8ba188317c82887910baf8dd1e
Gustavo Pasqua <dev.gustavo@outlook.com>
2024-05-26 16:35:55 -0300

    fix: Atualizando token do instagram que se encontrava expirado

--- src/app/services/instagram.service.ts
+++ src/app/services/instagram.service.ts
@@ -8,7 +8,7 @@ import { InstaData } from '../modules/InstaData.module';
 })
 export class InstagramService {
 
-  private url:string = 'https://graph.instagram.com/me/media?access_token=IGQWROY2IwOG85ZAldPT0NibklubWoybGpWYnBtUXZAONzBqVzNFN21DMHZAqRE5GZAGhvT0p3c2R4VW1rYmY0b1RuX3NHV2FoakVXOTVBVXB4eXZA4anZAPdGF4LU1ObEoxenEzQzRpcjR5bDBPVDd2NWVSSm5ObWlnMXcZD&fields=media_url,media_type,caption,permalink'
+  private url:string = 'https://graph.instagram.com/me/media?access_token=IGQWRPTWRRQTk0NGlhVk9HaWtoeDJDV2ZABd3dacHVIOFhBTm9NVUw3WHBDZAmV5SlA3TlRfdUF0OHY3Qks2aHczbE9wX0s3WU9IM0ZALQVREQkY2SkFwZA2RHbjdMd3FnVVJZAYWxCa2RrUFNZAMFMwZAXJ5WXlmdzR3RUkZD&fields=media_url,media_type,caption,permalink'
 
   constructor(private httpCLient: HttpClient) {
     
