972a35943767bfe89643406baf034ee9b1103fe7
Gustavo Pasqua <dev.gustavo@outlook.com>
2024-06-08 17:18:53 -0300

    fix/feat: Iniciado desenvolvimento do service de login / Retirado console log da landing

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  logado = false;
+
+  constructor() { }
+
+  logar(){
+    
+  }
+
+}
