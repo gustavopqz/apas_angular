a74a396dc248b19640d19a857c1e3aa9aa04f5f8
Gustavo Pasqua <dev.gustavo@outlook.com>
2025-07-31 12:05:06 -0300

    Fix: Ajuste na url da API APAS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'http://localhost:9000',
+    apiBaseUrl: 'https://api-apas.sempreautomatize.com.br:9000',
     frontBaseUrl: 'https://apaserrinha.netlify.app/#/doacoes'
 };
